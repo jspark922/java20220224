61e867bcb8f0b9658196706b6ceb696672db1325
psjoon <jspark922@naver.com>
2022-03-07 16:48:06 +0900

    220307 learn java

--- src/chap99/programmers/Solution12950Test.java
+++ src/chap99/programmers/Solution12950Test.java
@@ -1,6 +1,7 @@
 package chap99.programmers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
